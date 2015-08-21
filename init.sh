5620f0185d76400e624b5a6aa5ceeb55c4c15923
Cheewai Lai <cheewai.lai@gmail.com>
2015-08-21 15:52:57 +0200

    Remove -e from init.sh so that repeated docker-compose up will not crash due to existing runuser

--- init.sh
+++ init.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 #
-# DO NOT MODIFY - this script gets overwritten by the run script
 # This script will execute as 'root' in the container
-#
-set -eux
+set -ux
 
 # Create runtime user
-adduser --home /var/www/pydap --shell /bin/bash --uid 1000 --disabled-password --gecos "" runuser
+adduser --home /var/www/pydap --shell /bin/bash --uid ${UID} --disabled-password --gecos "" runuser
 
 chown -R runuser.runuser /var/www/pydap
 
